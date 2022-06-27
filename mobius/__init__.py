e12077ea86f1d6fcfed8aad3e344c03ea3d730d3
Jerome Eberhardt <jerome.eberhardt@unibas.ch>
2022-06-27 16:49:44 +0200

    changed imports

--- mobius/__init__.py
+++ mobius/__init__.py
@@ -7,25 +7,26 @@
 from .forcefield import ForceField
 from .virtual_target import VirtualTarget
 
-from .acquisition_functions import expected_improvement, probability_of_improvement, greedy, random_improvement, parallel_acq
+from .acquisition_functions import ExpectedImprovement, ProbabilityOfImprovement, Greedy, RandomImprovement, parallel_acq
 from .mobius import Mobius
+from .sampler import PolymerSampler
 from .descriptors import Map4Fingerprint, SequenceDescriptors
-from .ga import SequenceGA, ScaffoldGA, GA, RandomGA
+from .ga import SequenceGA, ScaffoldGA, RandomGA
 from .surrogate_model import GPModel
 from .generators import monomers_scanning, alanine_scanning, random_monomers_scanning, properties_scanning, scrumbled_scanning
-from .helm_genetic_operators import HELMGeneticOperators, compute_probability_matrix
+from .helm_genetic_operators import HELMGeneticOperators
 from .kernels import TanimotoSimilarityKernel
 from .oracle import Oracle
 from .utils import affinity_binding_to_energy, energy_to_affinity_binding, plot_results, split_list_in_chunks, generate_random_peptides
 
 __all__ = ['VirtualTarget', 'ForceField',
-           'expected_improvement', 'probability_of_improvement', 'greedy', 'random_improvement', 'parallel_acq',
-           'Mobius',
-           'Map4Fingerprint', 'SequenceDescriptors',
-           'SequenceGA', 'ScaffoldGA', 'GA', 'RandomGA',
-           'GPModel',
-           'monomers_scanning', 'alanine_scanning', 'random_monomers_scanning', 'properties_scanning', 'scrumbled_scanning',
-           'HELMGeneticOperators', 'compute_probability_matrix',
-           'TanimotoSimilarityKernel',
            'Oracle',
+           'Mobius',
+           'PolymerSampler',
+           'GPModel',
+           'TanimotoSimilarityKernel',
+           'ExpectedImprovement', 'ProbabilityOfImprovement', 'Greedy', 'RandomImprovement', 'parallel_acq',
+           'Map4Fingerprint', 'SequenceDescriptors',
+           'HELMGeneticOperators', 'SequenceGA', 'ScaffoldGA', 'RandomGA',
+           'monomers_scanning', 'alanine_scanning', 'random_monomers_scanning', 'properties_scanning', 'scrumbled_scanning',
            'affinity_binding_to_energy', 'energy_to_affinity_binding', 'plot_results', 'split_list_in_chunks', 'generate_random_peptides']
