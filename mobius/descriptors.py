d44a5af1795a683a555f7ad9838347020f11b78f
Jerome Eberhardt <qksoneo@gmail.com>
2022-08-11 15:10:50 +0200

    Added SubsitutionMatrix class

--- mobius/descriptors.py
+++ mobius/descriptors.py
@@ -67,6 +67,8 @@ class MorganFingerprint:
 class SequenceDescriptors:
 
     def __init__(self, descriptors, input_type='helm'):
+        assert input_type.lower() in ['fasta', 'helm', 'smiles'], 'Format (%s) not handled. Please use FASTA, HELM or SMILES format.'
+
         self._descriptors = descriptors
         self._input_type = input_type
 
@@ -86,3 +88,53 @@ class SequenceDescriptors:
             transformed.append(tmp)
 
         return np.asarray(transformed)
+
+
+class SubstitutionMatrix:
+
+    def __init__(self, matrix_filename, input_type='helm'):
+        assert input_type.lower() in ['fasta', 'helm', 'smiles'], 'Format (%s) not handled. Please use FASTA, HELM or SMILES format.'
+
+        self._input_type = input_type
+        self._matrix = self._read_matrix(matrix_filename)
+
+    def _read_matrix(self, matrix_filename):
+        data = []
+        columns = None
+
+        with open(matrix_filename) as f:
+            lines = f.readlines()
+
+            for line in lines:
+                if not line.startswith('#'):
+                    if columns is None:
+                        columns = [x.strip() for x in line.split(' ') if x]
+                    else:
+                        data.append([x.strip() for x in line.split(' ') if x][1:])
+
+        data = np.array(data).astype(int)
+
+        df = pd.DataFrame(data=data, columns=columns, index=columns)
+        return df
+
+    @property
+    def shape(self):
+        return self._matrix.shape
+
+    def substitutes(self, monomer_symbol, include=False, reverse_order=False):
+        monomers_to_drop = ['X']
+
+        try:
+            substitutions = self._matrix.loc[monomer_symbol]
+        except KeyError:
+            error_msg = 'Error: Monomer symbol %s not present in the substitution matrix.' % monomer_symbol
+            raise KeyError(error_msg)
+
+        substitutions.sort_values(ascending=reverse_order, inplace=True)
+
+        if not include:
+            monomers_to_drop.append(monomer_symbol)
+
+        substitutions.drop(monomers_to_drop, inplace=True)
+        
+        return substitutions.index.values
