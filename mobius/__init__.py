4ee51d5a447f42b0cae081f764eab0ebceac032d
Jerome Eberhardt <qksoneo@gmail.com>
2023-03-24 09:22:40 +0100

    Forgot to commit __init__.py

--- mobius/__init__.py
+++ mobius/__init__.py
@@ -14,11 +14,11 @@ from .descriptors import MHFingerprint, Map4Fingerprint, MorganFingerprint, Sequ
 from .ga import SequenceGA, ParallelSequenceGA, ScaffoldGA, RandomGA
 from .surrogate_model import GPModel, DummyModel, RFModel
 from .generators import monomers_scanning, alanine_scanning, random_monomers_scanning, properties_scanning, scrumbled_scanning, homolog_scanning, SubstitutionMatrix
-from .helm_genetic_operators import HELMGeneticOperators
+from .genetic_operators import GeneticOperators
 from .kernels import TanimotoSimilarityKernel
 from .emulator import LinearPeptideEmulator, FindMe
 from .plotting import plot_results
-from .utils import parse_helm, build_helm_string, read_pssm_file
+from .utils import parse_helm, build_helm_string, get_scaffold_from_helm_string, read_pssm_file
 from .utils import affinity_binding_to_energy, energy_to_affinity_binding, generate_random_linear_peptides, ic50_to_pic50, pic50_to_ic50
 from .utils import convert_FASTA_to_HELM, convert_HELM_to_FASTA
 
@@ -30,9 +30,11 @@ __all__ = ['VirtualTarget', 'ForceField',
            'TanimotoSimilarityKernel',
            'ExpectedImprovement', 'ProbabilityOfImprovement', 'Greedy', 'RandomImprovement',
            'MHFingerprint', 'Map4Fingerprint', 'MorganFingerprint', 'SequenceDescriptors',
-           'HELMGeneticOperators', 'SequenceGA', 'ScaffoldGA', 'RandomGA', 'ParallelSequenceGA',
-           'monomers_scanning', 'alanine_scanning', 'random_monomers_scanning', 'properties_scanning', 'scrumbled_scanning', 'homolog_scanning', 'SubstitutionMatrix',
+           'GeneticOperators', 'SequenceGA', 'ScaffoldGA', 'RandomGA', 'ParallelSequenceGA',
+           'monomers_scanning', 'alanine_scanning', 'random_monomers_scanning', 'properties_scanning', 
+           'scrumbled_scanning', 'homolog_scanning', 'SubstitutionMatrix',
            'plot_results',
-           'parse_helm', 'build_helm_string', 'read_pssm_file', 
-           'affinity_binding_to_energy', 'energy_to_affinity_binding', 'generate_random_linear_peptides', 'ic50_to_pic50', 'pic50_to_ic50',
-           'onvert_FASTA_to_HELM', 'convert_HELM_to_FASTA']
+           'parse_helm', 'build_helm_string', 'get_scaffold_from_helm_string', 'read_pssm_file', 
+           'affinity_binding_to_energy', 'energy_to_affinity_binding', 'generate_random_linear_peptides', 
+           'ic50_to_pic50', 'pic50_to_ic50',
+           'convert_FASTA_to_HELM', 'convert_HELM_to_FASTA']
