17a25f149a161dc3eb2019391da739e38f9005af
Jerome Eberhardt <qksoneo@gmail.com>
2023-05-30 21:20:01 +0200

    Use simple/complex HELM notation, and other small improvements.

--- mobius/genetic_operators.py
+++ mobius/genetic_operators.py
@@ -32,24 +32,24 @@ class GeneticOperators:
                                    ('SourceMonomerPosition', 'i4'), ('SourceAttachment', 'U2'),
                                    ('TargetMonomerPosition', 'i4'), ('TargetAttachment', 'U2')]
 
-    def mutate(self, input_sequence, scaffold_designs, n=10, 
+    def mutate(self, polymer, scaffold_designs, n=10, 
                minimum_mutations=1, maximum_mutations=None, keep_connections=True):
         """
-        Apply the mutation genetic operator on the input sequence.
+        Apply the mutation genetic operator on the input polymer.
 
         Parameters
         ----------
-        input_sequence : str
-            Input sequence in HELM format.
+        polymer : str
+            Polymer in HELM format.
         scaffold_designs : dictionary
-            Dictionary with scaffold sequences and defined set of monomers to 
+            Dictionary with scaffold polymers and defined set of monomers to 
             use for each position.
         n : int, default : 10
-            Total number of sequences to generate.
+            Total number of polymers to generate.
         minimum_mutations : int, default : 1
-            The minimum number of monomers to mutate in each sequence.
+            The minimum number of monomers to mutate in each polymer.
         maximum_mutations : int, default : None
-            The maximum number of monomers to mutate in each sequence.
+            The maximum number of monomers to mutate in each polymer.
         keep_connections : bool, default : True
             If True, monomers involved in a connection won't be mutated. If False, 
             monomers involved in a connection can be mutated. As consequence, the
@@ -58,44 +58,46 @@ class GeneticOperators:
         Returns
         -------
         List of str
-            Mutated sequences in HELM format.
+            Mutated polymers in HELM format.
 
         """
-        mutant_sequences = []
+        mutant_polymers = []
 
-        scaffold = get_scaffold_from_helm_string(input_sequence)
+        scaffold = get_scaffold_from_helm_string(polymer)
         assert scaffold in scaffold_designs, 'Scaffold %s not found in the scaffold designs.' % scaffold
 
         # Get the scaffold design for the current scaffold
         scaffold_design = scaffold_designs[scaffold]
         
-        polymers, connections, _, _ = parse_helm(input_sequence)
+        complex_polymer, connections, _, _ = parse_helm(polymer)
 
         # Generate mutants...
         for i in range(n):
-            mutant_polymers = {}
+            mutant_complex_polymer = {}
             n_mutations = 0
 
-            for pid, sequence in polymers.items():
-                mutated_sequence = list(sequence)
+            for pid, simple_polymer in complex_polymer.items():
+                mutated_simple_polymer = list(simple_polymer)
 
                 # Residues involved in a connection within and between peptides won't be mutated
-                if keep_connections and pid in polymers.keys():
+                if keep_connections and pid in complex_polymer.keys():
                     connection_resids = list(connections[connections['SourcePolymerID'] == pid]['SourceMonomerPosition'])
                     connection_resids += list(connections[connections['TargetPolymerID'] == pid]['TargetMonomerPosition'])
                     # -1, because positions are 1-based in HELM
                     connection_resids = np.asarray(connection_resids) - 1
-                    possible_positions = list(set(range(len(sequence))).difference(connection_resids))
+                    possible_positions = list(set(range(len(simple_polymer))).difference(connection_resids))
                 else:
-                    possible_positions = list(range(len(sequence)))
+                    possible_positions = list(range(len(simple_polymer)))
 
                 # Choose a random number of mutations between min and max
                 if minimum_mutations == maximum_mutations:
                     number_mutations = maximum_mutations
                 elif maximum_mutations is None:
-                    number_mutations = self._rng.integers(low=minimum_mutations, high=len(sequence))
+                    number_mutations = self._rng.integers(low=minimum_mutations, high=len(simple_polymer))
                 else:
-                    number_mutations = self._rng.integers(low=minimum_mutations, high=maximum_mutations)
+                    # The maximum number of mutations cannot be greater than the length of the polymer
+                    tmp_maximum_mutations = np.min([maximum_mutations, len(simple_polymer)])
+                    number_mutations = self._rng.integers(low=minimum_mutations, high=tmp_maximum_mutations)
 
                 # Choose positions to mutate
                 mutation_positions = self._rng.choice(possible_positions, size=number_mutations, replace=False)
@@ -104,9 +106,9 @@ class GeneticOperators:
                 for mutation_position in mutation_positions:
                     # +1 , because positions are 1-based in HELM
                     chosen_monomer = self._rng.choice(scaffold_design[pid][mutation_position + 1])
-                    mutated_sequence[mutation_position] = chosen_monomer
+                    mutated_simple_polymer[mutation_position] = chosen_monomer
 
-                mutant_polymers[pid] = (mutated_sequence, mutation_positions)
+                mutant_complex_polymer[pid] = (mutated_simple_polymer, mutation_positions)
                 n_mutations += len(mutation_positions)
 
             if n_mutations > 0:
@@ -117,78 +119,73 @@ class GeneticOperators:
                     for i, connection in enumerate(connections):
                         # The connection positions must not be in the mutation lists
                         # mutant_polymers[connection['XXXXXPolymerID']][1] + 1 because positions are 1-based in HELM
-                        if connection['SourceMonomerPosition'] not in mutant_polymers[connection['SourcePolymerID']][1] + 1 and \
-                           connection['TargetMonomerPosition'] not in mutant_polymers[connection['TargetPolymerID']][1] + 1:
+                        if connection['SourceMonomerPosition'] not in mutant_complex_polymer[connection['SourcePolymerID']][1] + 1 and \
+                           connection['TargetMonomerPosition'] not in mutant_complex_polymer[connection['TargetPolymerID']][1] + 1:
                             connections_to_keep.append(i)
                 else:
                     connections_to_keep = list(range(connections.shape[0]))
 
                 # Reconstruct the HELM string
-                mutant_sequence = build_helm_string({p: s[0] for p, s in mutant_polymers.items()}, connections[connections_to_keep])
-                mutant_sequences.append(mutant_sequence)
+                mutant_polymer = build_helm_string({p: s[0] for p, s in mutant_complex_polymer.items()}, connections[connections_to_keep])
+                mutant_polymers.append(mutant_polymer)
             else:
-                mutant_sequences.append(input_sequence)
+                mutant_polymers.append(polymer)
         
-        return mutant_sequences
+        return mutant_polymers
 
-    def crossover(self, input_sequence1, input_sequence2, cx_points=2):
+    def crossover(self, polymer1, polymer2, cx_points=2):
         """
-        Apply crossover genetic operator on the input sequence.
+        Apply crossover genetic operator on the two polymers.
 
         Parameters
         ----------
-        input_sequence1 : str
-            The first parent sequence in HELM format.
-        input_sequence2 : str
-            The second parent sequence in HELM format.
+        polymer1 : str
+            The first parent polymer in HELM format.
+        polymer2 : str
+            The second parent polymer in HELM format.
         cx_points : int, default : 2
             The number of crossover points.
 
         Returns
         -------
-        List of str
-            Two mutated sequences (children) in HELM format.
+        Tuple of str
+            Two mutated polymers (children) in HELM format.
 
         Raises
         ------
         ValueError
-            If polymers in the two input HELM strings have different polymer ids or different lengths, 
-            or if the scaffold (connections) in the two input HELM strings are different.
+            If polymers in the two input HELM strings have different simple polymer ids or 
+            different lengths, or if the scaffold (connections) in the two input HELM strings 
+            are different.
 
         """
-        mutant1_polymers = {}
-        mutant2_polymers = {}
-        mutant_sequences = []
+        mutant_complex_polymer1 = {}
+        mutant_complex_polymer2 = {}
 
-        polymers1, connections1, _, _ = parse_helm(input_sequence1)
-        polymers2, connections2, _, _ = parse_helm(input_sequence2)
+        scaffold1 = get_scaffold_from_helm_string(polymer1)
+        scaffold2 = get_scaffold_from_helm_string(polymer2)
+        assert scaffold1 == scaffold2, f'Polymers must have the same scaffold ({scaffold1} != {scaffold2}).)'
 
-        if polymers1.keys() != polymers2.keys():
-            raise ValueError('Polymers do not contain the same polymer ids.')
+        complex_polymer1, connections1, _, _ = parse_helm(polymer1)
+        complex_polymer2, connections2, _, _ = parse_helm(polymer2)
 
-        if connections1 != connections2:
-            raise ValueError('Polymers must have the same scaffold (connections are different).')
-
-        for pid in polymers1.keys():
-            if len(polymers1[pid]) != len(polymers2[pid]):
-                raise ValueError('Polymer sequences with ID %s have different lengths.' % pid)
-
+        for pid in complex_polymer1.keys():
             # Copy parents since we are going to modify the children
-            ind1 = list(polymers1[pid])
-            ind2 = list(polymers2[pid])
+            simple_polymer1 = list(complex_polymer1[pid])
+            simple_polymer2 = list(complex_polymer2[pid])
 
             # Choose positions to crossover
-            possible_positions = list(range(len(polymers1[pid])))
+            possible_positions = list(range(len(complex_polymer1[pid])))
             cx_positions = self._rng.choice(possible_positions, size=cx_points, replace=False)
             cx_positions = np.sort(cx_positions)
 
             for cx_position in cx_positions:
-                ind1[cx_position:], ind2[cx_position:] = ind2[cx_position:], ind1[cx_position:]
+                simple_polymer1[cx_position:], simple_polymer2[cx_position:] = simple_polymer2[cx_position:], simple_polymer1[cx_position:]
 
-            mutant1_polymers[pid] = ''.join(ind1)
-            mutant2_polymers[pid] = ''.join(ind2)
+            mutant_complex_polymer1[pid] = ''.join(simple_polymer1)
+            mutant_complex_polymer2[pid] = ''.join(simple_polymer2)
 
-        mutant_sequences.extend([build_helm_string(mutant1_polymers, connections1),
-                                 build_helm_string(mutant2_polymers, connections2)])
+        mutant_polymer1 = build_helm_string(mutant_complex_polymer1, connections1)
+        mutant_polymer2 = build_helm_string(mutant_complex_polymer2, connections2)
 
-        return mutant_sequences
+        return (mutant_polymer1, mutant_polymer2)
