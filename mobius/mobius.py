0382e5208bd9295828220e6e02697807ca8b66b5
Jerome Eberhardt <qksoneo@gmail.com>
2024-01-12 10:17:39 +0100

    Fixed noise bug

--- mobius/mobius.py
+++ mobius/mobius.py
@@ -17,38 +17,38 @@ class Mobius:
     def __init__(self):
         pass
     
-    def run(self, polymers, values, emulators, planner, num_iter=5, batch_size=96):
+    def run(self, sequences, values, emulators, planner, num_iter=5, batch_size=96):
         """
         Function for running the benchmark optimization process for polymers 
         against one or multiple `Emulator` (oracle).
 
         Parameters
         ----------
-            polymers : list of str
-                Polymers in HELM format.
+            sequences : list of str
+                Sequences in HELM format.
             values : list of int or float
-                Values associated to each polymer.
+                Values associated to each sequence.
             emulators : `_Emulator` or list of `_Emulator`
                 Emulator(s) (oracle) used to simulate actual lab experiments.
             planner : `mobius.Planner`
-                Protocol used for optimizing the polymer sequence space.
+                Protocol used for optimizing the sequence sequence space.
             num_iter : int, default: 5
                 Total number of optimization cycles.
             batch_size : int, default: 96
-                Size of the batches, number of polymers returned after 
+                Size of the batches, number of sequences returned after 
                 each optimization cycle
 
         Returns
         -------
         results : pd.DataFrame
             Pandas DataFrame containing all the results from the optimization 
-            process with the following columns: ['iter', 'polymer', 'exp_value', 
+            process with the following columns: ['iter', 'sequence', 'exp_value', 
             'pred_value']
 
         """
         data = []
 
-        all_suggested_polymers = np.asarray(polymers).copy()
+        all_suggested_sequences = np.asarray(sequences).copy()
         all_exp_values = np.asarray(values).copy()
 
         if all_exp_values.ndim == 1:
@@ -59,22 +59,22 @@ class Mobius:
                 "if it contains a single sample.".format(self._values)
             )
 
-        # number of values per polymers
+        # number of values per sequences
         n = all_exp_values.shape[1]
 
         if not isinstance(emulators, (list, tuple)):
             emulators = [emulators]
 
         # Add initial population
-        for p, ev in zip(all_suggested_polymers, all_exp_values):
+        for p, ev in zip(all_suggested_sequences, all_exp_values):
             data.append((0, p, *ev, *[np.nan] * n))
 
         for i in range(num_iter):
-            suggested_polymers, predicted_values = planner.recommand(all_suggested_polymers, all_exp_values, batch_size)
+            suggested_sequences, predicted_values = planner.recommand(all_suggested_sequences, all_exp_values, None, batch_size)
 
             exp_values = []
             for emulator in emulators:
-                values = np.asarray(emulator.score(suggested_polymers))
+                values = np.asarray(emulator.score(suggested_sequences))
 
                 if values.ndim == 1:
                     values = values.reshape(-1, 1)
@@ -82,13 +82,13 @@ class Mobius:
 
             exp_values = np.hstack(exp_values)
 
-            all_suggested_polymers = np.concatenate([all_suggested_polymers, suggested_polymers])
+            all_suggested_sequences = np.concatenate([all_suggested_sequences, suggested_sequences])
             all_exp_values = np.vstack([all_exp_values, exp_values])
 
-            for p, ev, pv in zip(suggested_polymers, exp_values, predicted_values):
+            for p, ev, pv in zip(suggested_sequences, exp_values, predicted_values):
                 data.append((i + 1, p, *ev, *pv))
 
-        columns = ['iter', 'polymer']
+        columns = ['iter', 'sequence']
         if n > 1:
             columns += [f'exp_value_{i + 1}' for i in range(n)]
             columns += [f'pred_value_{i + 1}' for i in range(n)]
@@ -99,17 +99,17 @@ class Mobius:
 
         return df
 
-    def benchmark(self, polymers, values, emulators, planners, num_iter=5, batch_size=96, num_independent_run=5):
+    def benchmark(self, sequences, values, emulators, planners, num_iter=5, batch_size=96, num_independent_run=5):
         """
-        Function to benchmark multiple sampling strategies for polymers/peptide 
+        Function to benchmark multiple sampling strategies for sequences/peptide 
         optimization against the `Emulator` (oracle).
 
         Parameters
         ----------
-            polymers : list of str
-                Polymers in HELM format.
+            sequences : list of str
+                Sequences in HELM format.
             values : list of int or float
-                Values associated to each polymer.
+                Values associated to each sequence.
             emulators : `Emulator` or list of `_Emulator`
                 Emulator (oracle) used to simulate actual lab experiments.
             planners : dict of `Planner`
@@ -118,7 +118,7 @@ class Mobius:
             num_iter : int, default: 5
                 Total number of optimization cycles.
             batch_size : int, default: 96
-                Size of the batches, number of polymers returned after 
+                Size of the batches, number of sequences returned after 
                 each optimization cycle.
             num_independent_run :, int, default: 5
                 Total number of independent runs to execute for each sampling 
@@ -128,23 +128,23 @@ class Mobius:
         -------
         results : pd.DataFrame
             Pandas DataFrame containing all the results from the optimization process with
-            the following columns: ['planner', 'run', 'iter', 'polymer', 'exp_value', 'pred_value']
+            the following columns: ['planner', 'run', 'iter', 'sequence', 'exp_value', 'pred_value']
 
         """
         dfs = []
 
         assert isinstance(planners, dict), 'Samplers must be defined as a dictionary ({\'sampler1_name\': sampler})'
 
-        all_suggested_polymers = np.asarray(polymers).copy()
+        all_suggested_sequences = np.asarray(sequences).copy()
         all_exp_values = np.asarray(values).copy()
 
         if not isinstance(emulators, (list, tuple)):
             emulators = [emulators]
 
-        # number of values per polymers
+        # number of values per sequences
         n = all_exp_values.shape[1]
 
-        columns = ['planner', 'run', 'iter', 'polymer']
+        columns = ['planner', 'run', 'iter', 'sequence']
         if n > 1:
             columns += [f'exp_value_{i + 1}' for i in range(n)]
             columns += [f'pred_value_{i + 1}' for i in range(n)]
@@ -153,7 +153,7 @@ class Mobius:
 
         for planner_name, sampler in planners.items():
             for i in range(num_independent_run):
-                df = self.run(all_suggested_polymers, all_exp_values, emulators, sampler, num_iter, batch_size)
+                df = self.run(all_suggested_sequences, all_exp_values, emulators, sampler, num_iter, batch_size)
 
                 df['run'] = i + 1
                 df['planner'] = planner_name
