16f072c36cbabf8e6f2bfe1a7814ac824eed8e46
Jérôme Eberhardt <jerome.eberhardt@unibas.ch>
2024-07-09 13:22:07 +0200

    Does now what is suppose to do; still need better repack selection.

--- mobius/damietta.py
+++ mobius/damietta.py
@@ -15,9 +15,9 @@ import tempfile
 import numpy as np
 import parmed as pmd
 from pdbfixer.pdbfixer import PDBFixer
-from openmm.app import ForceField, NoCutoff, HBonds, Simulation
+from openmm.app import ForceField, NoCutoff, HBonds, Simulation, PDBFile
 from openmm import VerletIntegrator, unit, Platform
-from prody import parsePDB, writePDB
+from prody import parsePDB, writePDB, HierView
 
 
 @contextlib.contextmanager
@@ -45,7 +45,10 @@ def execute_command(cmd_line):
 
 def run_sinai_cs_f2m2f(spec_input_filename, damietta_path):
     command_line = f'{damietta_path}/bin/sinai_cs_f2m2f {spec_input_filename} '
-    outputs, _ = execute_command(command_line)
+    outputs, errors = execute_command(command_line)
+
+    print(outputs)
+    print(errors)
 
     if 'failed to parse combinatorial sampling (cs) specs file' in outputs:
         raise RuntimeError(f'Command: {command_line} failed with error message: {outputs}')
@@ -158,10 +161,6 @@ class DamiettaScorer:
         self._residue_to_indices = {f'{r.getChid()}:{r.getResnum()}': i + 1 for i, r in enumerate(self._pdb.iterResidues())}
         self._indices_to_residue = {v: k for k, v in self._residue_to_indices.items()}
 
-        # Renumbers the residues in the pdb file, starting from 1
-        for i, residue in enumerate(self._pdb.iterResidues()):
-            residue.setResnum(i + 1)
-
     def minimize(self, max_iterations=100, platform='CPU', clean=True):
         """
         Minimizes the protein/peptide.
@@ -224,6 +223,57 @@ class DamiettaScorer:
             self._pdb = parsePDB(minimized_pdb_filename)
             #self._pdb.setCoords(state.getPositions(asNumpy=True).value_in_unit(unit.angstrom))
 
+    def _mutate_and_rebuild_sidechain(self, pdb, mutations):
+        pH = 7.0
+        tmp_pdbfilename = 'tmp.pdb'
+        selection_str = []
+
+        # To have access to residue
+        hv_pdb = HierView(pdb)
+
+        original_resnums = []
+        original_chainids = []
+
+        # PDBFixer is renumbering resids and changing chainids
+        for residue in pdb.iterResidues():
+            original_resnums.append(residue.getResnum())
+            original_chainids.append(residue.getChids()[0])
+
+        writePDB(tmp_pdbfilename, pdb, renumber=False)
+    
+        for mutation in mutations:
+            chainid, resid, new_resname = mutation.split(':')
+            residue_str = f'{chainid}:{resid}'
+            
+            selection_str.append(f'(resid {self._residue_to_indices[residue_str]} and sidechain)')
+    
+            # Change resname to the new one
+            residue = hv_pdb.getResidue(chainid, self._residue_to_indices[residue_str], segname=chainid)
+            residue.setResname(new_resname)
+    
+        # Select all except sidechains of the mutated residues
+        selection_str = 'not (' + ' or '.join(selection_str) + ')'
+        pdb_no_sc = pdb.select(selection_str).copy()
+    
+        writePDB(tmp_pdbfilename, pdb_no_sc, renumber=False)
+
+        # This part is slow.
+        fixer = PDBFixer(filename=tmp_pdbfilename)
+        fixer.findMissingResidues()
+        fixer.findMissingAtoms()
+        fixer.addMissingAtoms()
+        fixer.addMissingHydrogens(pH)
+        PDBFile.writeFile(fixer.topology, fixer.positions, open(tmp_pdbfilename, 'w'))
+    
+        mutated_pdb = parsePDB(tmp_pdbfilename)
+
+        # Put back original resids and chainids
+        for i, residue in enumerate(mutated_pdb.iterResidues()):
+            residue.setResnum(original_resnums[i])
+            residue.setChids([original_chainids[i]] * len(residue.getNames()))
+
+        return mutated_pdb
+
     def mutate_score(self, mutations, repack_neighbor_residues=False, neighbor_cutoff=4.0, load_memory=True, clean=True):
         """
         Mutates the protein/peptide with the given mutations list.
@@ -267,9 +317,14 @@ class DamiettaScorer:
         rpk_res_lines = ''
         out_dir = 'run'
         input_pdb_filename = 'protein.pdb'
+        mutation_resids = []
 
         # Create a copy of the pdb file
         pdb = self._pdb.copy()
+
+        # Renumbers the residues in the pdb file, starting from 1
+        residue_indices = pdb.getResindices()
+        pdb.setResnums(residue_indices + 1)
 
         # Create the input file for Damietta
         spec_input_str = f"library\t{self._damietta_lib_path}\n"
@@ -281,11 +336,18 @@ class DamiettaScorer:
 
         for mutation in mutations:
             chainid, resid, new_resname = mutation.split(':')
+
+            assert len(new_resname) == 3, 'Residue name must be 3-letter code.'
+
             residue = f'{chainid}:{resid}'
+
             try:
                 mut_res_lines += f"mut_res\t{self._residue_to_indices[residue]}\t{new_resname}\n"
             except KeyError:
                 raise RuntimeError(f'Residue {residue} not found in the pdb file.')
+
+            # Save this for later, to extract the energies from the output PDB
+            mutation_resids.append(self._residue_to_indices[residue])
 
         spec_input_str += mut_res_lines
         spec_input_str += "\n"
@@ -330,9 +392,10 @@ class DamiettaScorer:
 
         # Run Damietta
         with temporary_directory(prefix='dm_mut_', dir='.', clean=clean) as tmp_dir:
-            # Renumbers the residues in the pdb file, starting from 1
-            residue_indices = pdb.getResindices()
-            pdb.setResnums(residue_indices + 1)
+            # Mutate PDB outside Damietta, because we are going to trick sinai_cs_f2m2f
+            # to just score just the mutations we want, and nothing else.
+            # This part is slow, should find something else then PDBFixer
+            pdb = self._mutate_and_rebuild_sidechain(pdb, mutations)
 
             # Writes the pdb file
             writePDB(input_pdb_filename, pdb, renumber=False)
@@ -347,16 +410,28 @@ class DamiettaScorer:
 
             # Read the output pdb file
             with open(f'{out_dir}/init.pdb', 'r') as f:
-                while True:
-                    line = f.readline()
+                energies = []
 
-                    if line.startswith('REMARK AVERAGE ENERGY PER RESIDUE:'):
+                for line in f.readlines():
+                    # We do not take the total energy (REMARK AVERAGE ENERGY PER RESIDUE), 
+                    # but only the individual energy of each mutation, and average them
+                    # In REMARK AVERAGE ENERGY PER RESIDUE, we have all the mutated residues + the
+                    # ones that were repacked. Here we just want the mutated ones.
+                    if line.startswith('REMARK resid'):
                         sl = line.split()
-                        energies = np.array([float(sl[16]), float(sl[6]), float(sl[8]), float(sl[10]), float(sl[12]), float(sl[14])])
-                        break
+                        resid = int(sl[2])
+
+                        if resid in mutation_resids:
+                            energies.append([float(sl[14]), float(sl[4]), float(sl[6]), float(sl[8]), float(sl[10]), float(sl[12])])
+
+            energies = np.mean(energies, axis=0)
 
             # Replace the pdb with the mutated one
             self._pdb = parsePDB('run/init.pdb')
+
+        # Put back the original residue numbers
+        for i, residue in enumerate(self._pdb.iterResidues()):
+            residue.setResnum(self._indices_to_residue[i + 1].split(':')[1])
 
         return energies
 
@@ -370,10 +445,4 @@ class DamiettaScorer:
             The filename of the output pdb file.
 
         """
-        output_pdb = self._pdb.copy()
-
-        # Put back the original residue numbers
-        for i, residue in enumerate(output_pdb.iterResidues()):
-            residue.setResnum(self._indices_to_residue[i + 1].split(':')[1])
-
-        writePDB(output_pdb_filename, output_pdb)
+        writePDB(output_pdb_filename, self._pdb)
