a36690958cb531987ec58a27a0063cae924d69ac
Jérôme Eberhardt <jerome.eberhardt@unibas.ch>
2024-11-04 15:01:44 +0100

    Replace UFF by MMFF for the last optimization.

--- mobius/structure/vina.py
+++ mobius/structure/vina.py
@@ -59,7 +59,8 @@ def constrained_embed_multiple_confs(query_mol, core_mol, num_confs=10, cluster=
 
     """
     # Set up the ETKDG parameters
-    ps = rdDistGeom.ETKDGv3()
+    ps = AllChem.ETKDGv3()
+    ps.numThreads = 0
     ps.randomSeed = random_seed
     ps.useRandomCoords = True
     ps.useBasicKnowledge = True
@@ -70,12 +71,10 @@ def constrained_embed_multiple_confs(query_mol, core_mol, num_confs=10, cluster=
     ps.clearConfs = True
 
     # Forcefield-related parameters
-    getForceField = AllChem.UFFGetMoleculeForceField
     force_constant = 1e4
     force_tolerance = 1e-3
     energy_tolerance = 1e-4
-    force_constant_final = 0.1
-    max_displacement = 1.5
+    max_displacement = 1.0
 
     # Find common substructure between probe and reference
     mcs = rdFMCS.FindMCS([query_mol, core_mol], timeout=int(mcs_timeout))
@@ -85,7 +84,7 @@ def constrained_embed_multiple_confs(query_mol, core_mol, num_confs=10, cluster=
     core_match = core_mol.GetSubstructMatch(patt)
     cmap = [(i, j) for i, j in zip(query_match, core_match)]
 
-    cids = rdDistGeom.EmbedMultipleConfs(query_mol, num_confs, ps)
+    cids = AllChem.EmbedMultipleConfs(query_mol, num_confs, ps)
     cids = list(cids)
 
     if len(cids) == 0:
@@ -99,8 +98,8 @@ def constrained_embed_multiple_confs(query_mol, core_mol, num_confs=10, cluster=
         # Superpose the query onto the core
         rms_ini = rdMolAlign.AlignMol(query_mol, core_mol, atomMap=cmap, prbCid=cid)
 
-        # Do first minimization to superpose all the query atoms on the top of the core atoms
-        ff = getForceField(query_mol, confId=cid)
+        # Do first minimization with UFF to superpose all the query atoms on the top of the core atoms
+        ff = AllChem.UFFGetMoleculeForceField(query_mol, confId=cid)
 
         for atom_pair in cmap:
             p = core_conf.GetAtomPosition(atom_pair[1])
@@ -113,12 +112,13 @@ def constrained_embed_multiple_confs(query_mol, core_mol, num_confs=10, cluster=
             more = ff.Minimize(energyTol=energy_tolerance, forceTol=force_tolerance)
             n -= 1
 
-        # Do a last minimization to relax the whole query molecule without
+        # Do a last minimization with MMFF to relax the whole query molecule without
         # deviating too far from the initial placement
-        ff = getForceField(query_mol, confId=cid)
+        mp = AllChem.MMFFGetMoleculeProperties(query_mol)
+        ff = AllChem.MMFFGetMoleculeForceField(query_mol, mp, confId=cid)
 
         for atom_pair in cmap:
-            ff.UFFAddPositionConstraint(atom_pair[0], max_displacement, force_constant_final)
+            ff.MMFFAddPositionConstraint(atom_pair[0], max_displacement, force_constant)
 
         ff.Initialize()
         ff.Minimize(energyTol=energy_tolerance, forceTol=force_tolerance)
@@ -199,23 +199,25 @@ class VinaScorer:
         output_pdbqt = 'docker_tmp.pdbqt'
 
         if adjust_protonation:
+            input_molecule = Chem.RemoveHs(input_molecule)
             input_molecule = self._protonator(input_molecule, pH, pH)[0]
+            input_molecule = Chem.AddHs(input_molecule, addCoords=True)
 
         if template is not None:
             all_scores = []
             all_pdbqt_mols = []
 
             # Ignore warning because hydrogen atoms are missing
-            RDLogger.DisableLog('rdApp.warning')
+            #RDLogger.DisableLog('rdApp.warning')
 
             mols = constrained_embed_multiple_confs(input_molecule, template, num_confs=num_confs, cluster=cluster)
                 
-            RDLogger.EnableLog('rdApp.warning')
+            #RDLogger.EnableLog('rdApp.warning')
 
             if mols is None:
                 return None, np.array([np.nan] * 8)
 
-            for mol in mols:
+            for i, mol in enumerate(mols):
                 mol_setup = self._preparator.prepare(mol)[0]
                 pdbqt_string = PDBQTWriterLegacy.write_string(mol_setup)[0]
 
@@ -237,7 +239,7 @@ class VinaScorer:
             pdbqt_mol = all_pdbqt_mols[best_mol_id]
 
             # Cleanup temp pdbqt file
-            os.remove(output_pdbqt)
+            #os.remove(output_pdbqt)
         else:    
             mol_setup = self._preparator.prepare(input_molecule)[0]
             pdbqt_string = PDBQTWriterLegacy.write_string(mol_setup)[0]
